395244d89f2f7ed4ed18d538955d9714f3eb792c
keeganjones <keegs112@gmail.com>
2020-05-13 20:09:37 -0500

    Use SSL to take advantage of all of Max's hard work

--- src/axiosApi.js
+++ src/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://intro-env-edge.eba-6z5r73tg.us-east-2.elasticbeanstalk.com/intro'
+  baseURL: 'https://intro-env-edge.eba-6z5r73tg.us-east-2.elasticbeanstalk.com/intro'
 });
 
 export const withoutLoaderConfig = { withoutLoader: true };
