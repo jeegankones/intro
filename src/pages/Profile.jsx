1755e6b4659c8a1ad234e2755888dcd65fc3bda6
keeganjones <keegs112@gmail.com>
2020-06-17 12:36:07 -0500

    Fix logout bug

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,10 +4,14 @@ import { useAuth0 } from '../react-auth0-spa';
 const Profile = () => {
   const { isAuthenticated, logout } = useAuth0();
 
+  const handleLogout = () => {
+    logout({ returnTo: window.location.origin });
+  };
+
   return (
     <div className="container">
       <div>Profile page works!</div>
-      {isAuthenticated && <button type="button" className="btn btn-secondary" onClick={() => logout()}>Log out</button>}
+      {isAuthenticated && <button type="button" className="btn btn-secondary" onClick={handleLogout}>Log out</button>}
     </div>
   );
 };
